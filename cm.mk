e769e26cf67a5c03fea260776249bbe937951e83
Paletrox-Z <paletroxzpz@gmail.com>
2016-09-08 12:54:04 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := X3
+PRODUCT_RELEASE_NAME := x3a40
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Lenovo/X3/device_X3.mk)
+$(call inherit-product, device/Lenovo/x3a40/device_x3a40.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := X3
-PRODUCT_NAME := cm_X3
+PRODUCT_DEVICE := x3a40
+PRODUCT_NAME := cm_x3a40
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Lenovo X3
+PRODUCT_MODEL := Lenovo x3a40
 PRODUCT_MANUFACTURER := Lenovo
